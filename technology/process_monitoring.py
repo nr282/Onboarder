2ce1cd1d9ed92495cfb2c7b279c94f215b5df50d
Nathaniel Rogalskyj <nathaniel.rogalskyj100@gmail.com>
2026-05-25 15:16:58 -0400

    Add processing monitoring

---
+++ technology/process_monitoring.py
@@ -0,0 +1,6 @@
+"""
+A core primitive that was discovered is the ability to monitor child processes
+from the parent process in python and guide it to a particular state by providing
+input commands.
+
+"""
